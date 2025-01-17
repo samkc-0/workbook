81d0030a33f07b11cf7aacdbdd73144ee53aca68
samkc-0 <sam.k.c.0@icloud.com>
2025-01-17 18:53:53 +0100

    style matrices and add labels

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,107 +1,104 @@
 import { Matrix3, Matrix2, Vector2, Vector3, Vector4 } from "three";
-import { getDimension, parseMatrix, toArray, toLatex } from "./App";
+import {
+  dotproduct,
+  getDimension,
+  parseMatrix,
+  stringifyMatrix,
+  toArray,
+  toLatex,
+} from "./App";
 import { describe, expect, it } from "vitest";
 
 describe("parseMatrix can parse a...", () => {
-  it("2D vector", () => {
-    let result = parseMatrix("1 0") as Vector2;
-    expect(result).not.toBeUndefined();
-    expect(result.x).toBe(1);
-    expect(result.y).toBe(0);
-    result = parseMatrix("1;0") as Vector2;
-    expect(result).not.toBeUndefined();
-    expect(result.x).toBe(1);
-    expect(result.y).toBe(0);
-  });
-
-  it("3D vector", () => {
-    let result = parseMatrix("1 0 0") as Vector3;
-    expect(result).not.toBeUndefined();
-    expect(result.x).toBe(1);
-    expect(result.y).toBe(0);
-    expect(result.z).toBe(0);
-    result = parseMatrix("1;0;0") as Vector3;
-    expect(result).not.toBeUndefined();
-    expect(result.x).toBe(1);
-    expect(result.y).toBe(0);
-    expect(result.z).toBe(0);
-  });
-
-  it("4D vector", () => {
-    let result = parseMatrix("1 0 0 1") as Vector4;
-    expect(result).not.toBeUndefined();
-    expect(result.x).toBe(1);
-    expect(result.y).toBe(0);
-    expect(result.z).toBe(0);
-    expect(result.w).toBe(1);
-    result = parseMatrix("1;0;0;1") as Vector4;
-    expect(result).not.toBeUndefined();
-    expect(result.x).toBe(1);
-    expect(result.y).toBe(0);
-    expect(result.z).toBe(0);
-    expect(result.w).toBe(1);
-  });
-
   it("2D matrix", () => {
-    const result = parseMatrix("1 0; 0 1;") as Matrix2;
-    expect(result).not.toBeUndefined();
-    expect(result.elements[0]).toBe(1);
-    expect(result.elements[3]).toBe(1);
+    const result = parseMatrix("1 0; 0 1;");
+    expect(result[0][0]).toBe(1);
+    expect(result[1][1]).toBe(1);
   });
 
   it("3D matrix", () => {
-    const result = parseMatrix("1 0 0; 0 1 0; 0 0 1;") as Matrix3;
+    const result = parseMatrix("1 0 0; 0 1 0; 0 0 1;");
     expect(result).not.toBeUndefined();
-    expect(result.elements[0]).toBe(1);
-    expect(result.elements[4]).toBe(1);
-    expect(result.elements[8]).toBe(1);
+    expect(result[0][0]).toBe(1);
+    expect(result[1][1]).toBe(1);
+    expect(result[2][2]).toBe(1);
   });
 
   it("4D matrix", () => {
-    const result = parseMatrix(
-      "1 0 0 0; 0 1 0 0 ; 0 0 1 0; 0 0 0 1;"
-    ) as Matrix4;
+    const result = parseMatrix("1 0 0 0; 0 1 0 0 ; 0 0 1 0; 0 0 0 1;");
     expect(result).not.toBeUndefined();
-    expect(result.elements[0]).toBe(1);
-    expect(result.elements[5]).toBe(1);
-    expect(result.elements[10]).toBe(1);
-    expect(result.elements[15]).toBe(1);
+    expect(result[0][0]).toBe(1);
+    expect(result[1][1]).toBe(1);
+    expect(result[2][2]).toBe(1);
+    expect(result[3][3]).toBe(1);
   });
 });
 
-describe("toLatex can convert to latex a...", () => {
-  it("2D vector", () => {
-    const v = new Vector2().fromArray([1, 2]);
-    let latex = toLatex(v);
-    expect(latex).toBe(`\\begin{bmatrix} 1 & 2 \\end{bmatrix}`);
-
-    latex = toLatex(v, "transpose");
-    expect(latex).toBe(`\\begin{bmatrix} 1 \\\\ 2 \\end{bmatrix}`);
-  });
-  it("2D matrix", () => {
-    const latex = toLatex(new Matrix2().fromArray([1, 2, 3, 4]));
-    expect(latex).toBe(`\\begin{bmatrix} 1 & 2 \\\\ 3 & 4 \\end{bmatrix}`);
-  });
-});
-
-describe("toArray", () => {
-  it("converts a matrix to a 2D array", () => {
-    const m2 = new Matrix2();
-    expect(toArray(m2)).toEqual([
-      [1, 0],
-      [0, 1],
-    ]);
-  });
-  it("converts a vector to a 1D array", () => {
-    const v2 = new Vector2(1, 0);
-    expect(toArray(v2)).toEqual([1, 0]);
+describe("dotproduct", () => {
+  it("can compute the dotproduct for two 3x3 matrices", () => {
+    const a = [
+      [1, 2, 3],
+      [4, 5, 6],
+      [7, 8, 9],
+    ];
+    const b = [
+      [1, 1, 1],
+      [1, 1, 1],
+      [1, 1, 1],
+    ];
+    const expected = [
+      [6, 6, 6],
+      [15, 15, 15],
+      [24, 24, 24],
+    ];
+    expect(dotproduct(a, b)).toEqual(expected);
   });
 });
 
-it("getDimension gets a matrix dimension in the format 'mxn'", () => {
-  const result = getDimension([
-    [0, 1],
-    [1, 0],
-  ]);
-  expect(result).toBe("2x2");
+describe("stringifyMatrix", () => {
+  it("can put a matrix in string form", () => {
+    const a = [
+      [1, 2, 3],
+      [4, 5, 6],
+      [7, 8, 9],
+    ];
+    expect(stringifyMatrix(a)).toBe("1 2 3;4 5 6;7 8 9");
+  });
 });
+
+// describe("toLatex can convert to latex a...", () => {
+//   it("2D vector", () => {
+//     const v = new Vector2().fromArray([1, 2]);
+//     let latex = toLatex(v);
+//     expect(latex).toBe(`\\begin{bmatrix} 1 & 2 \\end{bmatrix}`);
+
+//     latex = toLatex(v, "transpose");
+//     expect(latex).toBe(`\\begin{bmatrix} 1 \\\\ 2 \\end{bmatrix}`);
+//   });
+//   it("2D matrix", () => {
+//     const latex = toLatex(new Matrix2().fromArray([1, 2, 3, 4]));
+//     expect(latex).toBe(`\\begin{bmatrix} 1 & 2 \\\\ 3 & 4 \\end{bmatrix}`);
+//   });
+// });
+
+// describe("toArray", () => {
+//   it("converts a matrix to a 2D array", () => {
+//     const m2 = new Matrix2();
+//     expect(toArray(m2)).toEqual([
+//       [1, 0],
+//       [0, 1],
+//     ]);
+//   });
+//   it("converts a vector to a 1D array", () => {
+//     const v2 = new Vector2(1, 0);
+//     expect(toArray(v2)).toEqual([1, 0]);
+//   });
+// });
+
+// it("getDimension gets a matrix dimension in the format 'mxn'", () => {
+//   const result = getDimension([
+//     [0, 1],
+//     [1, 0],
+//   ]);
+//   expect(result).toBe("2x2");
+// });
--- src/App.tsx
+++ src/App.tsx
@@ -1,146 +1,328 @@
-import { ReactNode } from "react";
+import { useEffect, useMemo, useState } from "react";
 import "./App.css";
 import { Vector2, Vector3, Vector4, Matrix2, Matrix3, Matrix4 } from "three";
 
 type MatmulProps = { A: string; B: string; product: string };
 type DisplayMatrixProps = { children: string; className: string };
-type Matrix = Matrix2 | Matrix3 | Matrix4;
-type Vector = Vector2 | Vector3 | Vector4;
+type ExplainProps = { prompt: string };
+type Matrix = { data: string; rows: number; columns: number };
 
-function App() {
+function App(): JSX.Element {
+  const A = { data: "1 2 3; 4 5 6; 7 8 9", rows: 3, columns: 3 };
+  const B = { data: "1 1 1; 2 2 2; 1 0 1", rows: 3, columns: 3 };
+  const ans = stringifyMatrix(dotproduct(parseMatrix(A), parseMatrix(B)));
   return (
-    <>
-      <Matmul
-        A={"1 0 0; 0 1 0; 0 0 1;"}
-        B={"2 2 2; 2 2 2; 2 2 2"}
-        product={"2 2 2; 2 2 2; 2 2 2"}
-      />
-    </>
+    <div className="matrix-grid">
+      <LabelMatrix A={A} B={B} />
+      <Matrix matrix={B} highlight="columns" className="B" />
+      <Matrix matrix={A} highlight={"rows"} className="A" />
+      <Matrix matrix={ans} highlight="cells" className="ans" />
+    </div>
   );
 }
 
-function Matmul({ A, B, product }: MatmulProps): JSX.Element {
+type MatrixProps = {
+  matrix: Matrix;
+  highlight: "rows" | "columns" | "cells";
+  className: string;
+};
+function Matrix({
+  matrix,
+  highlight,
+  className = "",
+}: MatrixProps): JSX.Element {
+  const arrayVersion = useMemo(() => parseMatrix(matrix), [matrix]);
   return (
-    <>
-      <DisplayMatrix className="A">{A}</DisplayMatrix>
-      <DisplayMatrix className="B">{B}</DisplayMatrix>
-      <DisplayMatrix className="ans">{product}</DisplayMatrix>
-    </>
+    <table className={`matrix ${className}`}>
+      <tbody>{display(arrayVersion)}</tbody>
+    </table>
+  );
+  function display(array2D: number[][]): JSX.Element[] {
+    return array2D.map((row: number[], i: number) => (
+      <tr key={`row-${i}`} className="row">
+        {row.map((cell: number, j: number) => {
+          let group = highlight;
+          switch (highlight) {
+            case "rows":
+              group += `-${i}`;
+              break;
+            case "columns":
+              group += `-${j}`;
+              break;
+            case "cells":
+              group += `-${i},${j}`;
+              break;
+            default:
+              throw new Error(`Invalid group: ${highlight}`);
+          }
+          return (
+            <td
+              key={`col-${j}`}
+              className={`cell ${group}`}
+              onMouseEnter={() => highlightGroup(group)}
+              onMouseLeave={() => unhighlightGroup(group)}
+            >
+              {cell}
+            </td>
+          );
+        })}
+      </tr>
+    ));
+  }
+
+  function highlightGroup(highlightClass: string) {
+    [...document.getElementsByClassName(highlightClass)].forEach((element) =>
+      element.classList.add("highlighted")
+    );
+  }
+  function unhighlightGroup(highlightClass: string) {
+    [...document.getElementsByClassName(highlightClass)].forEach((element) =>
+      element.classList.remove("highlighted")
+    );
+  }
+}
+
+function LabelMatrix({
+  A,
+  B,
+  labels = ["A", "B"],
+}: {
+  A: Matrix;
+  B: Matrix;
+  labels: string[];
+}): JSX.Element {
+  return (
+    <table className="label-matrix matrix">
+      <tbody>
+        {Array.from(new Array(B.rows)).map((_, i) => {
+          return (
+            <tr key={`labels-matrix-row-${i}`}>
+              {Array.from(new Array(A.columns)).map((_, j) => {
+                let content: string = "";
+                if (i === Math.floor(A.rows / 2) && j === B.columns - 1)
+                  content = labels[1];
+                if (i === A.columns - 1 && j === Math.floor(B.rows / 2))
+                  content = labels[0];
+                return (
+                  <td className="cell" key={`labels-matrix-cell-${i}-${j}`}>
+                    {content}
+                  </td>
+                );
+              })}
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
   );
 }
 
-function DisplayMatrix({
-  children,
-  className,
-}: DisplayMatrixProps): JSX.Element {
-  return <div className={className}>{toLatex(parseMatrix(children))}</div>;
-}
-
-function isMatrixOrVector(m: any): "vector" | "matrix" | undefined {
-  if (m.isVector2 || m.isVector3 || m.isVector4) return "vector";
-  if (m.isMatrix2 || m.isMatrix3 || m.isMatrix4) return "matrix";
-  return undefined;
-}
-
-export function parseMatrix(matrixString: string): Matrix | Vector | never {
-  // only handles 2d, 3d, and 4d vectors and matrices for now.
-  let rows: string[] | undefined = undefined;
-  if (!matrixString.includes(";")) {
-    // it might be a vector
-    rows = matrixString.split(" ");
-  }
-  if (matrixString.endsWith(";")) {
-    matrixString = matrixString.slice(0, -1);
-  }
-  if (rows == undefined) {
-    rows = matrixString.split(";");
-  }
-  const array2d: number[][] = rows.map((row) => {
-    const cells = row
+export function parseMatrix({ data }: Matrix): number[][] {
+  const rows = data.split(";").filter((row) => row.length !== 0);
+  const cells = rows.map((row) =>
+    row
       .trim()
       .split(" ")
-      .map((cell) => cell.trim());
-    return cells.map((cell: string) => Number(cell));
-  });
-  console.log(array2d);
-  const dimension = getDimension(array2d);
-  switch (dimension) {
-    case "1x2":
-    case "2x1":
-      return new Vector2(...array2d.flat());
-    case "1x3":
-    case "3x1":
-      return new Vector3(...array2d.flat());
-    case "1x4":
-    case "4x1":
-      return new Vector4().fromArray(array2d.flat());
-      break;
-    case "2x2":
-      return new Matrix2().fromArray(array2d.flat());
-    case "3x3":
-      return new Matrix3().fromArray(array2d.flat());
-    case "4x4":
-      return new Matrix4().fromArray(array2d.flat());
-    default:
-      break;
-  }
-  throw new Error(`Unimplemented matrix dimension: ${dimension}`);
+      .filter((cell) => cell.length !== 0)
+      .map(Number)
+  );
+  return cells;
 }
 
-export function getDimension(array2d: number[] | number[][]): string | never {
-  let [m, n] = [0, 0];
-  if (typeof array2d[0] === "number") {
-    m = 1;
-  } else {
-    m = array2d.length;
+export function dotproduct(A: number[][], B: number[][]) {
+  const m = A[0].length;
+  const n = B.length;
+  if (m != n) {
+    throw new Error(
+      "A has ${n} rows but B has ${n} columns, cannot compute dot product."
+    );
   }
-  if (m === 1) {
-    n = array2d.length;
-  } else if (typeof array2d[0] === "object") {
-    n = array2d[0].length;
-  } else {
-    throw new Error(`Invalid array2d: ${array2d.toString()}`);
-  }
-  return `${m}x${n}`;
-}
+  const result = Array.from({ length: A.length }, () =>
+    Array(B[0].length).fill(0)
+  );
 
-export function toArray(input: Vector | Matrix) {
-  switch (isMatrixOrVector(input)) {
-    case "vector":
-      return input.toArray();
-    case "matrix":
-      const dim = Math.sqrt(input.elements.length);
-      const out: number[][] = [];
-      for (let i = 0; i < dim; i++) {
-        out.push([]);
-        for (let j = 0; j < dim; j++) {
-          out[i].push(input.elements[i * dim + j]);
-        }
+  for (let i = 0; i < A.length; i++) {
+    for (let j = 0; j < B[0].length; j++) {
+      for (let k = 0; k < B.length; k++) {
+        result[i][j] += A[i][k] * B[k][j];
       }
-      return out;
-    default:
-      throw new Error(`Unsupported input ${input.toString()}`);
+    }
   }
+  return result;
 }
 
-export function toLatex(
-  input: Matrix | Vector,
-  transpose: false | "transpose" = false
-): string | never {
-  switch (isMatrixOrVector(input)) {
-    case "vector":
-      const vector = toArray(input);
-      const sep = transpose ? " \\\\ " : " & ";
-      let latexCells = vector.join(sep);
-      return "\\begin{bmatrix} " + latexCells + " \\end{bmatrix}";
-    case "matrix":
-      const matrix = toArray(input);
-      // Extract matrix elements
-      let latexRows = matrix.map((row: number[]) => row.join(" & "));
-      return "\\begin{bmatrix} " + latexRows.join(" \\\\ ") + " \\end{bmatrix}";
-    default:
-      throw new Error("The input must be a THREE.Matrix4 instance.");
-  }
+export function stringifyMatrix(matrix: number[][]) {
+  return {
+    data: matrix
+      .map((row) => {
+        return row.join(" ");
+      })
+      .join(";"),
+    rows: matrix.length,
+    columns: matrix[0].length,
+  };
 }
+
+// // this has to be implemented in the backend because otherwise it will use up all the allocation
+
+// // function Explain({ prompt }: ExplainProps): JSX.Element {
+// //   const [textContent, setTextContent] = useState("");
+// //   useEffect(() => {
+// //     const doFetch = async () => {
+// //       const url =
+// //         "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=YOUR_API_KEY";
+// //       const headers = { "Content-Type": "application/json" };
+// //       const method = "POST";
+// //       const body =
+// //         '{"contents": [{"parts":[{"text": "Write a story about a magic backpack."}]}]}';
+// //       await fetch(url, { method, headers, body })
+// //         .then((response) => {
+// //           return response.text();
+// //         })
+// //         .then((text) => {
+// //           setText(() => text);
+// //         });
+// //     };
+// //   }, []);
+// //   return <div className="explanation">{textContent}</div>;
+// }
+
+// function Matmul({ A, B, product }: MatmulProps): JSX.Element {
+//   return (
+//     <>
+//       <DisplayMatrix className="A">{A}</DisplayMatrix>
+//       <DisplayMatrix className="B">{B}</DisplayMatrix>
+//       <DisplayMatrix className="ans">{product}</DisplayMatrix>
+//     </>
+//   );
+// }
+
+// function DisplayMatrix({
+//   children,
+//   className,
+// }: DisplayMatrixProps): JSX.Element {
+//   useEffect(() => {
+//     if (typeof window?.MathJax !== "undefined") {
+//       window.MathJax.typeset();
+//     }
+//   }, []);
+//   return (
+//     <div className={className}>\( {toLatex(parseMatrix(children))} \)</div>
+//   );
+// }
+
+// // This has to be implemented differently.
+// // There should be a pair of matrices.
+// // One is highlighted rowwise, the other is highlighted columnwise.
+// // They are highlighted at the same time. This also highlights the cell that should be filled.
+// // to actually you need row-to-latex functions,
+// // also column-to-latex functions (harder).. maybe don't use latex.
+
+// function isMatrixOrVector(m: any): "vector" | "matrix" | undefined {
+//   if (m.isVector2 || m.isVector3 || m.isVector4) return "vector";
+//   if (m.isMatrix2 || m.isMatrix3 || m.isMatrix4) return "matrix";
+//   return undefined;
+// }
+
+// export function parseMatrix(matrixString: string): Matrix | Vector | never {
+//   // only handles 2d, 3d, and 4d vectors and matrices for now.
+//   let rows: string[] | undefined = undefined;
+//   if (!matrixString.includes(";")) {
+//     // it might be a vector
+//     rows = matrixString.split(" ");
+//   }
+//   if (matrixString.endsWith(";")) {
+//     matrixString = matrixString.slice(0, -1);
+//   }
+//   if (rows == undefined) {
+//     rows = matrixString.split(";");
+//   }
+//   const array2d: number[][] = rows.map((row) => {
+//     const cells = row
+//       .trim()
+//       .split(" ")
+//       .map((cell) => cell.trim());
+//     return cells.map((cell: string) => Number(cell));
+//   });
+//   console.log(array2d);
+//   const dimension = getDimension(array2d);
+//   switch (dimension) {
+//     case "1x2":
+//     case "2x1":
+//       return new Vector2(...array2d.flat());
+//     case "1x3":
+//     case "3x1":
+//       return new Vector3(...array2d.flat());
+//     case "1x4":
+//     case "4x1":
+//       return new Vector4().fromArray(array2d.flat());
+//       break;
+//     case "2x2":
+//       return new Matrix2().fromArray(array2d.flat());
+//     case "3x3":
+//       return new Matrix3().fromArray(array2d.flat());
+//     case "4x4":
+//       return new Matrix4().fromArray(array2d.flat());
+//     default:
+//       break;
+//   }
+//   throw new Error(`Unimplemented matrix dimension: ${dimension}`);
+// }
+
+// export function getDimension(array2d: number[] | number[][]): string | never {
+//   let [m, n] = [0, 0];
+//   if (typeof array2d[0] === "number") {
+//     m = 1;
+//   } else {
+//     m = array2d.length;
+//   }
+//   if (m === 1) {
+//     n = array2d.length;
+//   } else if (typeof array2d[0] === "object") {
+//     n = array2d[0].length;
+//   } else {
+//     throw new Error(`Invalid array2d: ${array2d.toString()}`);
+//   }
+//   return `${m}x${n}`;
+// }
+
+// export function toArray(input: Vector | Matrix) {
+//   switch (isMatrixOrVector(input)) {
+//     case "vector":
+//       return input.toArray();
+//     case "matrix":
+//       const dim = Math.sqrt(input.elements.length);
+//       const out: number[][] = [];
+//       for (let i = 0; i < dim; i++) {
+//         out.push([]);
+//         for (let j = 0; j < dim; j++) {
+//           out[i].push(input.elements[i * dim + j]);
+//         }
+//       }
+//       return out;
+//     default:
+//       throw new Error(`Unsupported input ${input.toString()}`);
+//   }
+// }
+
+// export function toLatex(
+//   input: Matrix | Vector,
+//   transpose: false | "transpose" = false
+// ): string | never {
+//   switch (isMatrixOrVector(input)) {
+//     case "vector":
+//       const vector = toArray(input);
+//       const sep = transpose ? " \\\\ " : " & ";
+//       let latexCells = vector.join(sep);
+//       return "\\begin{bmatrix} " + latexCells + " \\end{bmatrix}";
+//     case "matrix":
+//       const matrix = toArray(input);
+//       // Extract matrix elements
+//       let latexRows = matrix.map((row: number[]) => row.join(" & "));
+//       return "\\begin{bmatrix} " + latexRows.join(" \\\\ ") + " \\end{bmatrix}";
+//     default:
+//       throw new Error("The input must be a THREE.Matrix4 instance.");
+//   }
+// }
 
 export default App;
